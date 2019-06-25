b99435ccae917baf80cb7ebc366b17815ec8393a
Clifford Ng <wenloongng@gmail.com>
2019-06-25 13:33:04 -0700

    If Else test cases passed for array.sh and e-echo.cpp

--- array.sh
+++ array.sh
@@ -2,10 +2,27 @@
 
 INPUTS=("first input" "second input" "third input")
 
-echo "Executing ./e-echo ${INPUTS[2]}"
-./e-echo ${INPUTS[2]}
-echo "Executing ./e-echo ${INPUTS[1]}"
-./e-echo ${INPUTS[1]}
-echo "Executing ./e-echo ${INPUTS[0]}"
-./e-echo ${INPUTS[0]}
+#echo "Executing ./e-echo ${INPUTS[2]}"
+#./e-echo ${INPUTS[2]}
+#echo "Executing ./e-echo ${INPUTS[1]}"
+#./e-echo ${INPUTS[1]}
+#echo "Executing ./e-echo ${INPUTS[0]}"
+#./e-echo ${INPUTS[0]}
+#INPUTS=("first input" "second input" "third input")
+#!/bin/sh
 
+INPUTS=("first input" "second input" "third input")
+
+for input in "${INPUTS[@]}"
+do
+    echo "./e-echo ${input}"
+    output=$(./e-echo ${input})
+    echo "Output: \"${output}\""
+    echo "Input: \"${input}\""
+    if [ "${output}" = "${input}" ]
+    then
+        echo "Test passed"
+    else
+        echo "Test failed"
+    fi
+done
